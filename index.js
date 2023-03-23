37a031d7e7c13c609a422a49ad32d06dacbea0ee
williamelliott <ironmaiden229@hotmail.com>
2023-03-23 19:14:59 -0400

    project done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = ("BOB")
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = ("maybe bob")
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = "billy"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = ("maybe not billy")
+    return leastFavoriteCustomer
+}
